244e8ffdcea7e23e8a75d09993b44be4fffcad61
ArnavSaikia <arnavsaikia817@gmail.com>
2025-10-26 17:34:07 +0530

    ye tailwind thodi confusing to h

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,11 +1,27 @@
+import '../Styles/Navbar.css';
+import SearchIcon from '@mui/icons-material/Search';
+import AddIcon from '@mui/icons-material/Add';
+
 export default function Navbar() {
     return (
-        <div className="h-[7vh] w-[100vw] bg-gray-400">
-            <img src=""></img>
-            <a href="">Home</a>
-            <a href="">Categories</a>
-            <a href="">My Wardrobe</a>
-            <a href="">Outfit</a>
+        <div className="h-[7vh] w-[100vw] flex justify-between items-center border-b border-gray-400 font-[inter]">
+            <img src={null} className="hidden"></img>
+            <div className="flex gap-[1rem] justify-between items-center pl-[1rem] pr-[1rem]">
+                <a href="" className="navbar-link">Home</a>
+                <a href="" className="navbar-link">Categories</a>
+                <a href="" className="navbar-link">My Wardrobe</a>
+                <a href="" className="navbar-link">Outfit</a>
+            </div>
+            <div className='flex justify-between items-center'>
+                <button>
+                    <AddIcon style={{ fontSize: 28, color: "#333" }} />
+                    <span className='hidden'>Add Item</span>
+                </button>
+                <button>
+                    <SearchIcon style={{ fontSize: 28, color: "#333" }} />
+                    <span className='hidden'>Search</span>
+                </button>
+            </div>
         </div>
     )
 }
