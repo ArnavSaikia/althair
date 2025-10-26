b512ccdb70ee31975c349859be24f852dd30fe65
ArnavSaikia <arnavsaikia817@gmail.com>
2025-10-27 00:36:15 +0530

    respectable enough navbar. need to fix the buttons later according to the ui i wanna make

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -7,18 +7,18 @@ export default function Navbar() {
         <div className="h-[7vh] w-[100vw] flex justify-between items-center border-b border-gray-400 font-[inter]">
             <img src={null} className="hidden"></img>
             <div className="flex gap-[1rem] justify-between items-center pl-[1rem] pr-[1rem]">
-                <a href="" className="navbar-link">Home</a>
-                <a href="" className="navbar-link">Categories</a>
-                <a href="" className="navbar-link">My Wardrobe</a>
-                <a href="" className="navbar-link">Outfit</a>
+                <a href="" className="navbar-link group">Home<span></span></a>
+                <a href="" className="navbar-link group">Categories<span></span></a>
+                <a href="" className="navbar-link group">My Wardrobe<span></span></a>
+                <a href="" className="navbar-link group">Outfit<span></span></a>
             </div>
-            <div className='flex justify-between items-center'>
-                <button>
-                    <AddIcon style={{ fontSize: 28, color: "#333" }} />
+            <div className='flex justify-between items-center gap-[0rem]'>
+                <button className='shadow-md p-2 rounded'>
+                    <AddIcon style={{ fontSize: 16, color: "#333" }} />
                     <span className='hidden'>Add Item</span>
                 </button>
-                <button>
-                    <SearchIcon style={{ fontSize: 28, color: "#333" }} />
+                <button className='shadow-md p-2 rounded'>
+                    <SearchIcon style={{ fontSize: 16, color: "#333" }} />
                     <span className='hidden'>Search</span>
                 </button>
             </div>
