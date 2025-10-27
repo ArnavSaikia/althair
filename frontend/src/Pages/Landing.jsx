b492e4a9edc32f5e71cb34bd3a9a264a0979b2ce
ArnavSaikia <arnavsaikia817@gmail.com>
2025-10-27 23:33:53 +0530

    halfway through making the basic skeleton of the landing page

--- frontend/src/Pages/Landing.jsx
+++ frontend/src/Pages/Landing.jsx
@@ -2,7 +2,20 @@ import Navbar from '../Components/Navbar.jsx';
 
 function Landing(){
     return(
-        <Navbar/>
+        <>
+            <Navbar/>
+            <div className='size-max'>
+                <img src='../../public/hero.png' className='w-screen h-[40vh] object-cover'></img>
+                <div className='w-screen p-4'>
+                    <span className='text-lg'>Shop By Category</span>
+                    <div className='flex'>
+                        <img src={null}/>
+                        <img src={null}/>
+                        <img src={null}/>
+                    </div>
+                </div>
+            </div>
+        </> 
     )
 }
 
