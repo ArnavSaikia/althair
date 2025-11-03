2de3277e1589107676c28776edbbbe48d39c81b5
ArnavSaikia <arnavsaikia817@gmail.com>
2025-11-03 10:08:30 +0530

    completed the basic sketch of the category section

--- frontend/src/Pages/Landing.jsx
+++ frontend/src/Pages/Landing.jsx
@@ -8,10 +8,12 @@ function Landing(){
                 <img src='../../public/hero.png' className='w-screen h-[40vh] object-cover'></img>
                 <div className='w-screen p-4'>
                     <span className='text-lg'>Shop By Category</span>
-                    <div className='flex'>
-                        <img src={null}/>
-                        <img src={null}/>
-                        <img src={null}/>
+                    <div className='grid grid-cols-2 gap-4 '>
+                        <div className='flex flex-col gap-4'>
+                            <img src='../../public/upperwear_landing.jpg' className='h-[50%] flex-none aspect-2/3'/>
+                            <img src='../../public/bottomwear_landing.jpg' className='h-[50%] grow aspect-2/3'/>
+                        </div>
+                        <img src='../../public/footwear_landing.jpg' className='h-[100%] flex-none aspect-2/3'/>
                     </div>
                 </div>
             </div>
